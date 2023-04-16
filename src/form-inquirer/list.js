9996da91abae7f37400ce081f8ee20d703ed7828
Edhen Primasandy <edhen.primasandy@gmail.com>
2023-04-16 12:21:14 +0700

    fix(app): move form model
    Tidy up and create file form json for question of inquirer when create or update data

---
+++ src/form-inquirer/list.js
@@ -0,0 +1,28 @@
+const form = [
+  {
+    type : 'input',
+    name : 'komoditas',
+    message : 'Komoditas: ',
+  },
+  {
+    type : 'input',
+    name : 'area_provinsi',
+    message : 'Provinsi: '
+  },
+  {
+    type : 'input',
+    name : 'area_kota',
+    message : 'Kota: '
+  },
+  {
+    type : 'number',
+    name : 'size',
+    message : 'Size (number): '
+  },
+  {
+    type : 'number',
+    name : 'price',
+    message : 'Price (number): '
+  },
+];
+export default form;
